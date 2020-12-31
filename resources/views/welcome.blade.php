9d5d03e303e165987b1845944f953ec55242af32
Claude Kirke <claudekirke@gmail.com>
2021-01-01 00:51:18 +0800

    feat (frontend): install favicon set
    generated from https://realfavicongenerator.net/favicon_result?file_id=p1eqspgfr2t6s1hbm6011a621lnh6#.X-36tFMzZhE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png?v=1">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1">
+        <link rel="manifest" href="/site.webmanifest?v=1">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5">
+        <link rel="shortcut icon" href="/favicon.ico?v=1">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+
         <title>Laravel</title>
 
         <!-- Fonts -->
