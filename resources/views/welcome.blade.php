99a010e9a8db28594b3fb685f330877145bf0d8d
Claude Kirke <claudekirke@gmail.com>
2021-01-02 03:22:55 +0800

    feat (frontend) add welcome page content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.main')
 
 @section('content')
-    Hello world
+
+    <h1 class="text-8xl font-serif font-thin tracking-wider text-center mt-24">
+        Claude Kirke
+    </h1>
+    <section class="container p-24 space-y-6">
+        <p class="text-xl font-mono">
+            Hi there! It's great to have you as a visitor here.
+        </p>
+        <p class="text-xl font-mono">
+            I'm excited for you to see it, but this website is still under construction. Please check back again in a few days, or send me an email at
+            <a class="border-b" href="mailto:claudekirke@gmail.com">claudekirke@gmail.com</a>.
+        </p>
+        <p class="text-xl font-mono">
+            Thank you!
+        </p>
+        <div class="flex justify-around mx-96 py-16">
+            <img
+                src="/img/undraw_under_construction_46pa.svg"
+                alt="under construction"
+                title="Under construction! Check back again in a few days.">
+        </div>
+    </section>
+
 @endsection
