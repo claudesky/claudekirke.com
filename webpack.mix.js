8325dbdd344fe4cf0b9d384ea6d4da0acfc89570
Claude Kirke <claudekirke@gmail.com>
2021-01-01 12:50:15 +0800

    feat (webpack): configure laravel mix to output dev build to public/dev folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,15 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+if (process.env.NODE_ENV == 'production'){
+
+    mix.setPublicPath('public');
+    mix.version();
+
+} else if (process.env.NODE_ENV == 'development'){
+
+    mix.setPublicPath('public/dev');
+
+}
+
+mix.js('resources/js/app.js', 'js')
+    .postCss('resources/css/app.css', 'css');
