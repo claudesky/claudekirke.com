7d13547814a97fadefd7ec3e5bbd65f8ade97ce2
Claude Kirke <claudekirke@gmail.com>
2021-01-03 04:18:05 +0800

    feat (routes): give home route proper name

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/terms', function () {
     return view('pages.terms');
